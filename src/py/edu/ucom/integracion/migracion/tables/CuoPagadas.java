36ef5b834a8c4d40730c2411d0fee9eb818efbee
largonet <largonet@71a0fb67-4cdd-c169-41ea-e8dca69fe333>
2010-07-26 20:56:51 +0000

    Migracion de pagos lista

--- src/py/edu/ucom/integracion/migracion/tables/CuoPagadas.java
+++ src/py/edu/ucom/integracion/migracion/tables/CuoPagadas.java
@@ -14,65 +14,86 @@ import py.edu.ucom.integracion.migracion.ConexionServer;
  */
 public class CuoPagadas implements DatabaseTables {
 
-	public double monPagado;
-	public double nroCuoDesde;
-	public double nroCuoHasta;
-	public Pagos pagos;
-	public Cuotas cuotas;
-
-	public Pagos getPagos() {
-		return pagos;
-	}
-
-	public void setPagos(Pagos newPagos) {
-	}
-
-	public Cuotas getCuotas() {
-		return cuotas;
-	}
-
-	public void setCuotas(Cuotas newCuotas) {
-		if (this.cuotas == null) {
-			this.cuotas = new Cuotas();
-		} else if (!this.cuotas.equals(newCuotas)) {
-			this.cuotas = newCuotas;
-		}
-
-	}
-
-	public void save(ConexionServer server) {
-		throw new UnsupportedOperationException("Not supported yet.");
-	}
-
-	public void delete(ConexionServer server) {
-		throw new UnsupportedOperationException("Not supported yet.");
-	}
-
-	public void migrar(ConexionServer server) {
-		throw new UnsupportedOperationException("Not supported yet.");
-	}
-
+	private double monPagado;
+	private int nroCuoDesde;
+	private int nroCuoHasta;
+	private String codPago;
+	public String codCuota;
+	/**
+	 * @return the monPagado
+	 */
 	public double getMonPagado() {
 		return monPagado;
 	}
-
+	/**
+	 * @return the nroCuoDesde
+	 */
+	public int getNroCuoDesde() {
+		return nroCuoDesde;
+	}
+	/**
+	 * @return the nroCuoHasta
+	 */
+	public int getNroCuoHasta() {
+		return nroCuoHasta;
+	}
+	/**
+	 * @return the codPago
+	 */
+	public String getCodPago() {
+		return codPago;
+	}
+	/**
+	 * @return the codCuota
+	 */
+	public String getCodCuota() {
+		return codCuota;
+	}
+	/**
+	 * @param monPagado the monPagado to set
+	 */
 	public void setMonPagado(double monPagado) {
 		this.monPagado = monPagado;
 	}
-
-	public double getNroCuoDesde() {
-		return nroCuoDesde;
-	}
-
-	public void setNroCuoDesde(double nroCuoDesde) {
+	/**
+	 * @param nroCuoDesde the nroCuoDesde to set
+	 */
+	public void setNroCuoDesde(int nroCuoDesde) {
 		this.nroCuoDesde = nroCuoDesde;
 	}
-
-	public double getNroCuoHasta() {
-		return nroCuoHasta;
-	}
-
-	public void setNroCuoHasta(double nroCuoHasta) {
+	/**
+	 * @param nroCuoHasta the nroCuoHasta to set
+	 */
+	public void setNroCuoHasta(int nroCuoHasta) {
 		this.nroCuoHasta = nroCuoHasta;
 	}
+	/**
+	 * @param codPago the codPago to set
+	 */
+	public void setCodPago(String codPago) {
+		this.codPago = codPago;
+	}
+	/**
+	 * @param codCuota the codCuota to set
+	 */
+	public void setCodCuota(String codCuota) {
+		this.codCuota = codCuota;
+	}
+	@Override
+	public void delete(ConexionServer server) {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public void migrar(ConexionServer server) {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public void save(ConexionServer server) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	
 }
